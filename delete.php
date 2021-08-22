9c1e7dffe3f63a03501497f2797d7c360ef39dfd
nidhi-vainavi <69414662+nidhi-vainavi@users.noreply.github.com>
2021-08-22 11:35:33 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+    $con = mysqli_connect('localhost','root','','facebookclone');
+    $phoneoremail = $_POST['phoneoremail'];
+
+    $check = mysqli_query($con, "DELETE FROM userdata WHERE phoneoremail= '$phoneoremail'");
+
+    if($check > 0){
+     header('Location:http://localhost/facebookcloneboilerplate/');
+    }
+    ?>
